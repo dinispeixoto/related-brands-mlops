0f76d15ddbaf80bbd89e7b216fa6e2cb6a38c2d7
Dinis <dinispeixoto96@gmail.com>
2020-11-29 17:21:03 +0000

    updating pipeline

--- code/deploy/score.py
+++ code/deploy/score.py
@@ -1,44 +1,30 @@
-import os
-import joblib
+import json
 import numpy as np
-import argparse
-
-from sklearn.svm import SVC
-from azureml.core import Model
-from azureml.monitoring import ModelDataCollector
-from inference_schema.schema_decorators import input_schema, output_schema
-from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
+import os
+import pickle
+import joblib
 
 
-# The init() method is called once, when the web service starts up.
-# Typically you would deserialize the model file, as shown here using joblib,
-# and store it in a global variable so your run() method can access it later.
 def init():
-    global model
-    global inputs_dc, prediction_dc
-    # The AZUREML_MODEL_DIR environment variable indicates
-    # a directory containing the model file you registered.
-    model_file_name = "model.pkl"
-    model_path = os.path.join(os.environ.get("AZUREML_MODEL_DIR"), model_file_name)
-    model = joblib.load(model_path)
-    inputs_dc = ModelDataCollector("sample-model", designation="inputs", feature_names=["feat1", "feat2", "feat3", "feat4"])
-    prediction_dc = ModelDataCollector("sample-model", designation="predictions", feature_names=["prediction"])
+    global recommendations
+    recommendations_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'related_brands_recommendations.pkl')
+    recommendations = joblib.load(recommendations_path)
 
 
-# The run() method is called each time a request is made to the scoring API.
-# Shown here are the optional input_schema and output_schema decorators
-# from the inference-schema pip package. Using these decorators on your
-# run() method parses and validates the incoming payload against
-# the example input you provide here. This will also generate a Swagger
-# API document for your web service.
-@input_schema('data', NumpyParameterType(np.array([[0.1, 1.2, 2.3, 3.4]])))
-@output_schema(StandardPythonParameterType({'predict': [['Iris-virginica']]}))
-def run(data):
-    # Use the model object loaded by init().
-    result = model.predict(data)
-    inputs_dc.collect(data) #this call is saving our input data into Azure Blob
-    prediction_dc.collect(result) #this call is saving our input data into Azure Blob
+def run(raw_data):
 
-    # You can return any JSON-serializable object.
-    return { "predict": result.tolist() }
+    def recommend(id, num=5):
+        response = []
+        recs = recommendations[id]['recommendations'][:num]
+        for rec in recs:
+            brand_id = rec[1]
+            score = rec[0]
+            # brand_name = brand_descriptions_df.loc[brand_descriptions_df['brand_id'] == brand_id, 'brand_name'].iloc[0]
+            brand_name = recommendations[brand_id]['name']
+            response.append(f"Brand {brand_name} ({brand_id}) with score {str(score)}\n")
+        
+        return response
+
+    brand_id = json.loads(raw_data)['data'][0]
+
+    return recommend(brand_id)
--- code/train/train.py
+++ code/train/train.py
@@ -1,126 +1,66 @@
 import os
+import glob
+import time
+import joblib
 import argparse
-import itertools
+
 import numpy as np
 import pandas as pd
-import joblib
-import matplotlib.pyplot as plt
 
-from sklearn import datasets
-from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split
-
-from azureml.core import Dataset, Run
-run = Run.get_context()
-
-
-def log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix', title='Confusion matrix', cmap=plt.cm.Blues):
-    '''
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    '''
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
-        print('Normalized confusion matrix')
-    else:
-        print('Confusion matrix, without normalization')
-    print(cm)
-    
-    plt.figure()
-    plt.imshow(cm, interpolation = 'nearest', cmap = cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels, rotation = 45)
-    plt.yticks(tick_marks, labels)
-    
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment = "center", color = 'white' if cm[i, j] > thresh else 'black')
-    
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
-    run.log_image(log_name, plot=plt)
-    plt.savefig(os.path.join('outputs', '{0}.png'.format(log_name)))
-
-
-def log_confusion_matrix(cm, labels):
-    # log confusion matrix as object
-    cm_json =   {
-       'schema_type': 'confusion_matrix',
-       'schema_version': 'v1',
-       'data': {
-           'class_labels': labels,
-           'matrix': cm.tolist()
-       }
-    }
-    run.log_confusion_matrix('confusion_matrix', cm_json)
-    
-    # log confusion matrix as image
-    log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix_unnormalized', title='Confusion matrix')
-    
-    # log normalized confusion matrix as image
-    log_confusion_matrix_image(cm, labels, normalize=True, log_name='confusion_matrix_normalized', title='Normalized confusion matrix')
+from azureml.core import Run
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def main(args):
-    # create the outputs folder
+    path = args.data_folder
+    min_df = args.min_df
+    max_df = args.max_df
+
+    all_files = glob.glob(os.path.join(path, "*"))
+    print(all_files)
+
+    df_from_each_file = (pd.read_csv(file) for file in all_files)
+    brand_descriptions_df = pd.concat(df_from_each_file, ignore_index=True)
+
+    print(brand_descriptions_df.head(5))
+
+    run = Run.get_context()
+
+    print("Training model...")
+    start = time.time()
+
+    model = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=min_df, max_df=max_df, stop_words='english', )
+    tfidf_matrix = model.fit_transform(brand_descriptions_df['description'])
+
+    print("Training completed")
+    end = time.time()
+    run.log('time', end - start)
+
+    # Build Recommendations
+    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    recommendations = {} # dictionary created to store the result in a dictionary format (ID : (Score,item_id))#
+    for idx, row in brand_descriptions_df.iterrows(): #iterates through all the rows
+        # the below code 'similar_indice' stores similar ids based on cosine similarity. sorts them in ascending order. [:-5:-1] is then used so that the indices with most similarity are got. 0 means no similarity and 1 means perfect similarity#
+        similar_indices = cosine_similarities[idx].argsort()[:-15:-1] 
+        #stores 5 most similar books, you can change it as per your needs
+        similar_items = [(cosine_similarities[idx][i], brand_descriptions_df['brand_id'][i]) for i in similar_indices]
+        recommendations[row['brand_id']] = {'name': row['brand_name'], 'recommendations': similar_items[1:]}
+
+    print("Exporting model")
     os.makedirs('outputs', exist_ok=True)
-    
-    # Log arguments
-    run.log('Kernel type', np.str(args.kernel))
-    run.log('Penalty', np.float(args.penalty))
 
-    # Load iris dataset
-    X, y = datasets.load_iris(return_X_y=True)
-    
-    #dividing X,y into train and test data
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=223)
-    data = {'train': {'X': x_train, 'y': y_train},
-            'test': {'X': x_test, 'y': y_test}}
-    
-    # train a SVM classifier
-    svm_model = SVC(kernel=args.kernel, C=args.penalty, gamma='scale').fit(data['train']['X'], data['train']['y'])
-    svm_predictions = svm_model.predict(data['test']['X'])
-
-    # accuracy for X_test
-    accuracy = svm_model.score(data['test']['X'], data['test']['y'])
-    print('Accuracy of SVM classifier on test set: {:.2f}'.format(accuracy))
-    run.log('Accuracy', np.float(accuracy))
-    
-    # precision for X_test
-    precision = precision_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('Precision of SVM classifier on test set: {:.2f}'.format(precision))
-    run.log('precision', precision)
-    
-    # recall for X_test
-    recall = recall_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('Recall of SVM classifier on test set: {:.2f}'.format(recall))
-    run.log('recall', recall)
-    
-    # f1-score for X_test
-    f1 = f1_score(svm_predictions, data["test"]["y"], average='weighted')
-    print('F1-Score of SVM classifier on test set: {:.2f}'.format(f1))
-    run.log('f1-score', f1)
-    
-    # create a confusion matrix
-    labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-    labels_numbers = [0, 1, 2]
-    cm = confusion_matrix(y_test, svm_predictions, labels_numbers)
-    log_confusion_matrix(cm, labels)
-    
-    # files saved in the "outputs" folder are automatically uploaded into run history
-    model_file_name = "model.pkl"
-    joblib.dump(svm_model, os.path.join('outputs', model_file_name))
+    # joblib.dump(value=model, filename='outputs/related_brands_tfidf_model.pkl')
+    joblib.dump(value=recommendations, filename='outputs/related_brands_recommendations.pkl')
+    print("Training job finished")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--kernel', type=str, default='rbf', help='Kernel type to be used in the algorithm')
-    parser.add_argument('--penalty', type=float, default=1.0, help='Penalty parameter of the error term')
+    parser.add_argument('--data-folder', type=str, dest='data_folder', help='data folder mounting point')
+    parser.add_argument('--min-df', type=float, dest='min_df', default=1, help='min document frequency')
+    parser.add_argument('--max-df', type=float, dest='max_df', default=1.0, help='max document frenquency')
     args = parser.parse_args()
     return args
 
