19c641524a0f93b7ea483a6d9ffc19f6e9aa620f
Dinis <dinispeixoto96@gmail.com>
2020-11-29 13:43:55 +0000

    setup - train, monitor and register model

--- src/setup.py
+++ src/setup.py
@@ -2,14 +2,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-from azureml.core import Workspace, Experiment, Dataset
+from azureml.core import Workspace, Experiment, Dataset, ScriptRunConfig
 from azureml.core.compute import AmlCompute
 from azureml.core.compute import ComputeTarget
+from azureml.core.environment import Environment
+from azureml.core.conda_dependencies import CondaDependencies
 
 
 COMPUTE_NAME = 'githubcluster'
 DATASET_NAME = 'brand_descriptions'
 EXPERIMENT_NAME = 'related-brands'
+DATA_FOLDER = '../data/'
 
 # Load workspace configuration from the config.json file in the current folder
 # TO-DO: documentation how on to download the configuration file
@@ -26,15 +29,34 @@ if compute_target and type(compute_target) is AmlCompute:
 
 
 # Create a folder data/ where our dataset will be placed
-data_folder = os.path.join(os.getcwd(), '../data')
+data_folder = os.path.join(os.getcwd(), DATA_FOLDER)
 os.makedirs(data_folder, exist_ok=True)
 
 
 # Download the brand descriptions dataset 
-brand_descriptions_dataset = workspace.datasets[DATASET_NAME]
-brand_descriptions_dataset.to_csv_files(separator=',').download(target_path=data_folder, overwrite=True)
+brand_descriptions_tabular_dataset = workspace.datasets[DATASET_NAME]
+brand_descriptions_file_dataset = brand_descriptions_tabular_dataset.to_csv_files(separator=',')
 
+brand_descriptions_file_dataset.download(target_path=data_folder, overwrite=True)
 
-# Training script
-# script_folder = os.path.join(os.getcwd(), "train.py")
+# Setup the conda environment (environment.yml)
+environment = Environment('training_env')
+conda_dependencies = CondaDependencies.create(pip_packages=['azureml-dataset-runtime[pandas,fuse]', 'azureml-defaults'], conda_packages = ['scikit-learn==0.22.1'])
 
+environment.python.conda_dependencies = conda_dependencies
+environment.register(workspace=workspace)
+
+# Submit training job with train.py script
+script_folder = os.path.join(os.getcwd())
+
+src = ScriptRunConfig(source_directory=script_folder,
+                      script='train.py', 
+                      arguments=['--data-folder', brand_descriptions_file_dataset.as_mount()],
+                      compute_target=compute_target,
+                      environment=environment)
+
+run = experiment.submit(config=src)
+run.wait_for_completion(show_output=True) 
+
+model = run.register_model(model_name='related_brands', model_path='outputs/related_brands_tfidf_model.pkl')
+print(model.name, model.id, model.version, sep='\t')
--- src/train.py
+++ src/train.py
@@ -1,23 +1,49 @@
 import os
 import glob
+import time
+import joblib
 import argparse
 
 import numpy as np
 import pandas as pd
 
 from azureml.core import Run
-from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data-folder', type=str, dest='data_folder', help='data folder mounting point')
-# parser.add_argument('--regularization', type=float, dest='reg', default=0.01, help='regularization rate')
+parser.add_argument('--min-df', type=float, dest='min_df', default=1, help='min document frequency')
+parser.add_argument('--max-df', type=float, dest='max_df', default=1.0, help='max document frenquency')
+
+
 args = parser.parse_args()
 
 path = args.data_folder
+min_df = args.min_df
+max_df = args.max_df
+
 all_files = glob.glob(os.path.join(path, "*"))
+print(all_files)
 
 df_from_each_file = (pd.read_csv(file) for file in all_files)
-concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
+brand_descriptions_df = pd.concat(df_from_each_file, ignore_index=True)
 
-print(concatenated_df.head(5))
+print(brand_descriptions_df.head(5))
+
+run = Run.get_context()
+
+print("Training model...")
+start = time.time()
+
+model = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=min_df, max_df=max_df, stop_words='english', )
+model.fit(brand_descriptions_df['description']) # Transform missing
+
+print("Training completed")
+end = time.time()
+run.log('time', end - start)
+
+print("Exporting model")
+os.makedirs('outputs', exist_ok=True)
+joblib.dump(value=model, filename='outputs/related_brands_tfidf_model.pkl')
+
+print("Training job finished")
